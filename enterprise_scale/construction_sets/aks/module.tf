1850c6d7dc92d6bc6698364c652f1c9726e7fd00
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-03-01 14:45:37 +0800

    Update module.tf

--- enterprise_scale/construction_sets/aks/module.tf
+++ enterprise_scale/construction_sets/aks/module.tf
@@ -1,7 +1,6 @@
 module "caf" {
-  source = "github.com/aztfmod/terraform-azurerm-caf.git?ref=HN-ip-group"
-  # source  = "aztfmod/caf/azurerm"
-  # version = "~> 5.1.0"
+  source  = "aztfmod/caf/azurerm"
+  version = "~> 5.2.0"
 
   global_settings      = var.global_settings
   logged_user_objectId = var.logged_user_objectId
