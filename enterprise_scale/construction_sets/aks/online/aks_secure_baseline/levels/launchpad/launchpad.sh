a9506beb7560019ed04b00e6efb53f8366b67259
Eugene <eugene.fedor@gmail.com>
2021-03-25 14:19:29 -0700

    deploy

--- enterprise_scale/construction_sets/aks/online/aks_secure_baseline/levels/launchpad/launchpad.sh
+++ enterprise_scale/construction_sets/aks/online/aks_secure_baseline/levels/launchpad/launchpad.sh
@@ -4,5 +4,7 @@ id=$(az storage account list --query "[?tags.tfstate=='level0' && tags.launchpad
 
 if [ "${id}" == "null" ]; then 
     git clone https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/public
-    /tf/rover/rover.sh -lz /tf/caf/public/landingzones/caf_launchpad -a apply -launchpad -var-folder /tf/caf/enterprise_scale/construction_sets/aks/online/aks_secure_baseline/levels/launchpad
-fi    
+    /tf/rover/rover.sh -lz /tf/caf/public/landingzones/caf_launchpad -a apply -launchpad -var-folder /tf/caf/enterprise_scale/construction_sets/aks/online/aks_secure_baseline/levels/launchpad    
+fi  
+
+echo "STORAGE ID":${id}
