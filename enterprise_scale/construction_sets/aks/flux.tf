0bce70db6d44420321dce6894e4ea841e0692ab4
Eugene <eugene.fedor@gmail.com>
2021-04-08 14:28:09 -0700

    gh

--- enterprise_scale/construction_sets/aks/flux.tf
+++ enterprise_scale/construction_sets/aks/flux.tf
@@ -55,37 +55,3 @@ resource "kubernetes_secret" "fluxauth" {
 }
 
 
-data "kubectl_file_documents" "install" {
-  content = data.flux_install.main.content
-}
-
-data "kubectl_file_documents" "sync" {
-  content = data.flux_sync.main.content
-}
-
-locals {   
-
-  install = var.flux_namespace == "" ? null : [for v in data.kubectl_file_documents.install.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-  sync = var.flux_namespace == "" ? null : [for v in data.kubectl_file_documents.sync.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-resource "kubectl_manifest" "install" {
-  for_each   = var.flux_namespace == "" ? {} : { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux-system]
-  yaml_body  = each.value
-}
-
-resource "kubectl_manifest" "sync" {
-  for_each   = var.flux_namespace == "" ? {} : { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux-system]
-  yaml_body  = each.value
-}
-
