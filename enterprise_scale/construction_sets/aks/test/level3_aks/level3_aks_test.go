bfd8fbd1a782f095da7ebd45b8b42c0cb7d6305d
Eugene <eugene.fedor@gmail.com>
2021-04-12 17:21:17 -0700

    Test network policy

--- enterprise_scale/construction_sets/aks/test/level3_aks/level3_aks_test.go
+++ enterprise_scale/construction_sets/aks/test/level3_aks/level3_aks_test.go
@@ -60,8 +60,11 @@ func TestAksLoadBalancerProfile(t *testing.T) {
 
 	cluster := getCluster(t, expectedValues.ResourceGroupName, expectedValues.ClusterName)
 
-	// Test Network type (plugin) is Azure
+	// Test Network type (plugin)
 	assert.Equal(t, expectedValues.NetworkPlugin, string(cluster.NetworkProfile.NetworkPlugin))
+
+	// Test Network policy
+	assert.Equal(t, expectedValues.NetworkPolicy, string(cluster.NetworkProfile.NetworkPolicy))
 }
 
 func TestAksNetworkProfile(t *testing.T) {
