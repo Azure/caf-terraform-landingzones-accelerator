c1b1f918bc6ed4675a792983a95338086df35f87
Eugene <eugene.fedor@gmail.com>
2021-06-04 10:25:57 -0700

    fix flux

--- enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/deploy_level_with_rover.sh
+++ enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/deploy_level_with_rover.sh
@@ -19,5 +19,4 @@ git clone --branch azure_devops_v1 https://github.com/Azure/caf-terraform-landin
   -var-folder /tf/caf/enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/configuration/${LEVEL_NAME}/${LZ_NAME} \
   -tfstate ${LZ_NAME}.tfstate \
   -level ${LEVEL_NAME} \
-  -a apply \
-  -o rover.output
+  -a apply
