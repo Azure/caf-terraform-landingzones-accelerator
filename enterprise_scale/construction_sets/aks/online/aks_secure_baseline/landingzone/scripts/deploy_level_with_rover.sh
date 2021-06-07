36556f58f8ad73f3571ba8801e79c3a274be904b
Hieu Nguyen Nhu <5441003+hieumoscow@users.noreply.github.com>
2021-06-07 11:40:19 +0800

    Modify Pipeline

--- enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/deploy_level_with_rover.sh
+++ enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/deploy_level_with_rover.sh
@@ -7,16 +7,22 @@
 # e.g:
 # deploy_level_with_rover.sh level1 shared_services
 
+export TF_VAR_environment=${TF_VAR_environment:="sandpit"}
 LEVEL_NAME=$1
 LZ_NAME=$2
 ADDON_NAME=$3
+ACTION=$4
 
-# git clone https://github.com/aztfmod/terraform-azurerm-caf.git /tf/caf/public
-git clone --branch azure_devops_v1 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+if [ -d "/tf/caf/landingzones" ] 
+then
+  echo "/tf/caf/landingzones already exists"
+else
+  git clone --branch azure_devops_v1 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+fi
 
 /tf/rover/rover.sh  \
   -lz /tf/caf/landingzones/caf_solution${ADDON_NAME} \
   -var-folder /tf/caf/enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/configuration/${LEVEL_NAME}/${LZ_NAME} \
   -tfstate ${LZ_NAME}.tfstate \
   -level ${LEVEL_NAME} \
-  -a apply
+  -a ${ACTION}
--- enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/launchpad.sh
+++ enterprise_scale/construction_sets/aks/online/aks_secure_baseline/landingzone/scripts/launchpad.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 storage_name=$(az storage account list --query "[?tags.tfstate=='level0' && tags.launchpad=='launchpad']" -o json | jq -r .[0].name)
+export TF_VAR_environment=${TF_VAR_environment:="sandpit"}
+
 
 if [ "${storage_name}" = "null" ]; then
-     git clone --branch azure_devops_v1 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+    if [ -d "/tf/caf/landingzones" ] 
+    then
+        echo "/tf/caf/landingzones already exists"
+    else
+        git clone --branch azure_devops_v1 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+    fi
 
 
     # /tf/rover/rover.sh -lz /tf/caf/public/caf_launchpad -a apply -launchpad -var-folder /tf/caf/enterprise_scale/construction_sets/aks/online/aks_secure_baseline/configuration/launchpad
