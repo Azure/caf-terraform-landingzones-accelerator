0f2d3a12f16ee4ea5ca3b5605b884f727f6ded01
lolorol <lalesle@microsoft.com>
2021-07-19 01:52:34 +0000

    Update script

--- documentation/maintainer/start-docker-desktop.ps1
+++ documentation/maintainer/start-docker-desktop.ps1
@@ -1,5 +1,22 @@
 # Credits - https://stackoverflow.com/questions/52621129/restart-docker-windows-10-command-line/55212066#55212066
 
+Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Restarting docker"
+
+foreach($svc in (Get-Service | Where-Object {$_.name -ilike "*docker*" -and $_.Status -ieq "Running"}))
+{
+    $svc | Stop-Service -ErrorAction Continue -Confirm:$false -Force
+    $svc.WaitForStatus('Stopped','00:00:20')
+}
+
+Get-Process | Where-Object {$_.Name -ilike "*docker*"} | Stop-Process -ErrorAction Continue -Confirm:$false -Force
+
+foreach($svc in (Get-Service | Where-Object {$_.name -ilike "*docker*" -and $_.Status -ieq "Stopped"} ))
+{
+    $svc | Start-Service 
+    $svc.WaitForStatus('Running','00:00:20')
+}
+
+
 Write-Output "$((Get-Date).ToString("HH:mm:ss")) - Starting Docker Desktop"
 & "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 $startTimeout = [DateTime]::Now.AddSeconds(90)
